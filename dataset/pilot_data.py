7aefd5cb3ee275ab5b2400e2c90269b2cbc55af7
LS-xiaomi <lisen_work@163.com>
2020-03-23 09:26:04 +0800

    模型训练验证基本框架搭建完毕

--- dataset/pilot_data.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Author：sen
-# Date：2020/3/17 17:52
-
-
-import librosa
-
-def resample():
-    
--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 # Author：sen
-# Date：2020/3/22 15:47
+# Date：2020/3/22 10:47
